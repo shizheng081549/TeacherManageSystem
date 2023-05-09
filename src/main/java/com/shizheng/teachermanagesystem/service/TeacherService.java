e8fe30ed3de55fbd357f2d23d4aa361076069960
shizheng <947516613@qq.com>
2023-05-09 15:49:29 +0800

    教师管理系统

--- src/main/java/com/shizheng/teachermanagesystem/service/TeacherService.java
+++ src/main/java/com/shizheng/teachermanagesystem/service/TeacherService.java
@@ -2,7 +2,6 @@ package com.shizheng.teachermanagesystem.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
